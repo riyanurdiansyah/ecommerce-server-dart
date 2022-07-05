233e6f808dbbb48c2e535e3116763bd46adaf3f0
riyan <riyanurdiansyah@gmail.com>
2022-07-05 21:08:06 +0700

    publish #3

--- bin/server.dart
+++ bin/server.dart
@@ -4,11 +4,26 @@ import 'package:args/args.dart';
 import 'package:shelf/shelf.dart';
 
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_router/shelf_router.dart';
 
 import 'routes/app_route.dart';
 
 var portEnv = Platform.environment['PORT'];
 var hostname = portEnv == null ? 'localhost' : '0.0.0.0';
+
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/slug', AppRoute.fnHome);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
 
 void main(List<String> args) async {
   var parser = ArgParser()..addOption('port', abbr: 'p');
@@ -25,9 +40,8 @@ void main(List<String> args) async {
     return;
   }
 
-  var handler = const Pipeline()
-      .addMiddleware(logRequests())
-      .addHandler(AppRoute().routes);
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   var server = await io.serve(handler, hostname, port);
   print('Serving at http://${server.address.host}:${server.port}');
